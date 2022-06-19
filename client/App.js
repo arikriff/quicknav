030e3fe7d29186e5b1d74b6b77c28b6f5576f61d
arikriff <arikriff@gmail.com>
2022-06-19 21:01:17 +0300

    App File

--- client/App.js
+++ client/App.js
@@ -6,13 +6,12 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react'
+import React from 'react'
 import DataErrorScreen from './modules/component/visual/screen/DataErrorScreen'
 import JourneyPlanningScreen from './modules/component/visual/screen/JourneyPlanningScreen'
 import StopListScreen from './modules/component/visual/screen/StopListScreen'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { toCollege } from './modules/info/Direction'
 import { ContextProvider } from './modules/component/Context'
 import Data from './modules/info/Data'
 
@@ -23,11 +22,13 @@ const App = () => {
 
   return (
     <ContextProvider data={DATA}>
-      <Stack.Navigator initialRouteName={DATA ? 'JourneyPlanning' : 'DataError'}>
-        <Stack.Screen name='JourneyPlanning' component={JourneyPlanningScreen}/>
-        <Stack.Screen name='StopList' component={StopListScreen}/>
-        <Stack.Screen name='DataError' component={DataErrorScreen}/>
-      </Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={DATA ? 'JourneyPlanning' : 'DataError'}>
+          <Stack.Screen name='JourneyPlanning' component={JourneyPlanningScreen}/>
+          <Stack.Screen name='StopList' component={StopListScreen}/>
+          <Stack.Screen name='DataError' component={DataErrorScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </ContextProvider>
   )
 }
